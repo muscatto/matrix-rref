40805d37621033f8418413e978b849691e2f1360
muscatto <thasuno@outlook.jp>
2026-01-08 16:34:09 +0900

    Fractionクラスにコメント追加

--- src/fraction.js
+++ src/fraction.js
@@ -10,23 +10,29 @@ class Fraction {
     this.d = d / g;
   }
 
+  // 約分(ユーグリッドの互除法)
   static gcd(a, b) {
+    // 余りが0になるまで商で割り続ける
     while (b !== 0) [a, b] = [b, a % b];
     return a;
   }
 
+  // 足し算
   add(f) {
     return new Fraction(this.n * f.d + f.n * this.d, this.d * f.d);
   }
 
+  // 引き算
   sub(f) {
     return new Fraction(this.n * f.d - f.n * this.d, this.d * f.d);
   }
 
+  // 掛け算
   mul(f) {
     return new Fraction(this.n * f.n, this.d * f.d);
   }
 
+  // 割り算
   div(f) {
     return new Fraction(this.n * f.d, this.d * f.n);
   }
